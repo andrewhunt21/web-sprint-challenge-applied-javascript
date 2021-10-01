d7ac52ef08d20c5571a3392cc300da71440ccceb
andrewhunt21 <andrewhunt21@icloud.com>
2021-10-01 15:14:06 -0700

    broke task 3 cant figure out task 4

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -15,29 +17,29 @@ const Tabs = (topics) => {
   //
   const theTopics = document.createElement('div');
   const tab1 = document.createElement('div');
-  const tab2 = document.createElement('div');
-  const tab3 = document.createElement('div');
-  const tab4 = document.createElement('div');
-  const tab5 = document.createElement('div');
+  // const tab2 = document.createElement('div');
+  // const tab3 = document.createElement('div');
+  // const tab4 = document.createElement('div');
+  // const tab5 = document.createElement('div');
 
   theTopics.appendChild(tab1);
-  theTopics.appendChild(tab2);
-  theTopics.appendChild(tab3);
-  theTopics.appendChild(tab4);
-  theTopics.appendChild(tab5);
+  // theTopics.appendChild(tab2);
+  // theTopics.appendChild(tab3);
+  // theTopics.appendChild(tab4);
+  // theTopics.appendChild(tab5);
 
   theTopics.classList.add('topics');
   tab1.classList.add('tab');
-  tab2.classList.add('tab');
-  tab3.classList.add('tab');
-  tab4.classList.add('tab');
-  tab5.classList.add('tab');
+  // tab2.classList.add('tab');
+  // tab3.classList.add('tab');
+  // tab4.classList.add('tab');
+  // tab5.classList.add('tab');
 
-  tab1.textContent = topics[0];
-  tab2.textContent = topics[1];
-  tab3.textContent = topics[2];
-  tab4.textContent = topics[3];
-  tab5.textContent = topics[4];
+  tab1.textContent = topics;
+  // tab2.textContent = topics;
+  // tab3.textContent = topics;
+  // tab4.textContent = topics;
+  // tab5.textContent = topics;
 
   return theTopics;
 }
@@ -50,6 +52,20 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get('http://localhost:5000/api/topics')
+  .then(resp => {
+    for ( let i = 0; i < resp.data.topics.length; i++) {
+      const tabObj = {
+        topics: resp.data.topics[i],
+      }
+      const tabTopics = Tabs(tabObj);
+      document.querySelector(selector).appendChild(tabTopics);
+    }
+    console.log(resp.data.topics);
+  })
+  .catch(err => {
+    console.error(err);
+  })
 }
 
 export { Tabs, tabsAppender }
