90ccacc1369afc35aca5b12291349aa771d6413c
andrewhunt21 <andrewhunt21@icloud.com>
2021-10-01 16:39:39 -0700

    sort of got tasks 5 & 6 working as they can grab some of the info from one object but i am trying to figure out how to make them grab the objects nested in objects

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,6 @@
-const Card = (article) => {
+import axios from "axios";
+
+function Card(article) {
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -17,6 +19,37 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  // const headline = null;
+  // const authorPhoto = null;
+  // const authorName = null;
+
+  const theCard = document.createElement('div');
+  const theHeadline = document.createElement('div');
+  const theAuthor = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const theImg = document.createElement('img');
+  const theName = document.createElement('span');
+
+  theCard.appendChild(theHeadline);
+  theCard.appendChild(theAuthor);
+  theAuthor.appendChild(imgContainer);
+  imgContainer.appendChild(theImg);
+  theAuthor.appendChild(theName);
+
+  theCard.classList.add('card');
+  theHeadline.classList.add('headline');
+  theAuthor.classList.add('author');
+  imgContainer.classList.add('img-container');
+
+  theHeadline.textContent = `${article.headline}`;
+  theImg.src = `${article.authorPhoto}`;
+  theAuthor.textContent = `By ${article.authorName}`;
+
+  theCard.addEventListener('click', evt => {
+    console.log(evt.theHeadline);
+  })
+
+  return theCard;
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +61,22 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get('http://localhost:5000/api/articles')
+  .then(resp => {
+    for(let i = 0; i < resp.data.articles.javascript.length; i++) {
+      const articleObj = {
+        headline: resp.data.articles.javascript[i].headline,
+        authorPhoto: resp.data.articles.javascript[i].authorPhoto,
+        authorName: resp.data.articles.javascript[i].authorName
+      }
+      const theArticles = Card(articleObj);
+      document.querySelector(selector).appendChild(theArticles);
+    }
+    console.log(resp.data.articles);
+  })
+  .catch(err => {
+    console.error(err);
+  })
 }
 
 export { Card, cardAppender }
